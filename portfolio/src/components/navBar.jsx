e2da64d6c806376ca27f57612893461e256a8028
garrett griffey <garrettwgriffey@gmail.com>
2020-06-26 19:43:46 -0500

    set up skeleton

---
+++ portfolio/src/components/navBar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return <h1>Garrett Griffey</h1>;
+    }
+}
+ 
+export default NavBar;
---
+++ portfolio/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const element = <h1>Garrett Griffey</h1>;
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
