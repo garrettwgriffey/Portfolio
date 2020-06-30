61e1ce50c70b4c8d41fa059c7db605d4cd7ec3f2
garrett griffey <garrettwgriffey@gmail.com>
2020-06-29 20:37:09 -0500

    json populated

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 export default function Card(props) {
     return (
+        
         <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src={props.image} class="card-img-top" alt={props.title}>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">{props.title}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-        <a className="Card" href={props.link}>
-            <h2>{props.title}</h2>
-            <img src={props.image}/>
-            
-        </a>
-    )
-}
+    <a href={props.link} class="btn btn-primary">{props.title}</a>
+    </div>
+    
+  
+
+        
+    );
+};
 
 
 
